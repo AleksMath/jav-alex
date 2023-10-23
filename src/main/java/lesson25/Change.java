4459a232d10c06de7dd593debff2d88967efbaa1
Aleksei <aleksei.lapenkov@gmail.com>
2023-10-23 22:04:45 +0300

    init

---
+++ src/main/java/lesson25/Change.java
@@ -0,0 +1,10 @@
+package lesson25;
+
+public class Change {
+    public static void main(String[] args) {
+        String input = "Hello hou are you :( Im fine thanks";
+        String replace = input.replace(":(", ":)");
+        System.out.println(replace);
+    }
+
+}
